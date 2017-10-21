965033ebc94de05f9166490bb1c201c107d10ddb
Floofies <fantasticdragons@gmail.com>
2017-10-21 12:47:43 -0400

    General fixes

--- microVDom.js
+++ microVDom.js
@@ -1,5 +1,5 @@
 // microVDom - JavaScript Version
-(function () {
+var microDocument = (function () {
 	// Node Types
 	const ELEMENT_NODE = 1;
 	const TEXT_NODE = 3;
@@ -110,6 +110,7 @@
 	};
 	function element(tagName, attrMap = null) {
 		parentNode.call(this);
+		this.nodeType = ELEMENT_NODE;
 		this.attributes = [];
 		this.tagName = tagName;
 		this.selfClosing = SELF_CLOSING_TAGS.indexOf(tagName) !== -1;
@@ -117,6 +118,7 @@
 			attrMap.forEach((value, name) => this.setAttribute(value, name));
 		}
 	}
+	element.prototype = Object.create(parentNode.prototype);
 	element.prototype.setAttribute = function (attribute, value = "") {
 		if (attribute instanceof attr) {
 			this.attributes[attribute.name] = attribute;
@@ -128,32 +130,34 @@
 		var openTag = "<" + this.tagName;
 		if (this.attributes.length > 0) {
 			for (var attr of this.attributes) {
-				openTag =+ attr.render();
+				openTag += attr.render();
 			}
 		}
-		openTag =+ ">\n";
+		openTag += ">\n";
 		if (this.selfClosing) {
 			return openTag;
 		}
 		var contents = "";
 		if (this.children.length > 0) {
 			for (var childNode of this.children) {
-				contents =+ childNode.render();
+				contents += childNode.render();
 			}
 		}
 		var closeTag = "</" + this.tagName + ">\n";
 		return openTag + contents + closeTag;
-	}
+	};
 	function microDocument(docType = "html") {
 		parentNode.call(this);
 		this.characterSet = "UTF-8";
-		this.docType = new DOCUMENT_TYPE_NODE(docType);
+		this.docType = new documentType(docType);
 		this.nodeType = DOCUMENT_NODE;
 		this.documentElement = this.createElement("html");
 		this.head = this.createElement("head");
 		this.body = this.createElement("body");
+		this.documentElement.appendChild([this.head, this.body]);
 		this.appendChild([this.docType, this.documentElement]);
 	}
+	microDocument.prototype = Object.create(parentNode.prototype);
 	microDocument.prototype.createAttribute = function (name, value) {
 		return new attr(name, value);
 	};
@@ -172,8 +176,13 @@
 	microDocument.prototype.render = function () {
 		var html = "";
 		for (var childNode of this.children) {
-			html =+ childNode.render();
+			html += childNode.render();
 		}
 		return html;
 	};
+	return microDocument;
 })();
+// NodeJS `require` compatibility
+if (typeof module !== "undefined") {
+	module.exports = microDocument;
+}
